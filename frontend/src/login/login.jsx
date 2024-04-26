76923f64b35d97183162f44787217f7e0f533b12
unknown <darodriguesufc@gmail.com>
2024-04-26 15:09:48 -0300

    oi

--- frontend/src/login/login.jsx
+++ frontend/src/login/login.jsx
@@ -111,7 +111,7 @@ export function SignIn({isAuth,setIsAuth}) {
             </Button>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
+        {/* <Copyright sx={{ mt: 8, mb: 4 }} /> */}
       </Container>
     </ThemeProvider>
   );
